07e5e4f5c2fd042c179f2f6456e28e6f8edc200a
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-11-22 11:30:51 -0500

    Add data streaming scripts

---
+++ IOutils.py
@@ -0,0 +1,78 @@
+# Contains Input-Output Utilities
+# for handling data
+import numpy as np
+import pandas as pd
+
+
+RAW_CHANNEL_NAMES = ['Fp1','Fp2','F7','F3','Fz','F4','F8','FC5','FC1','FC2','FC6','T7','C3','Cz','C4','T8','TP9','CP5','CP1','CP2','CP6','TP10','P7','P3','Pz','P4','P8','PO9','O1','Oz','O2','PO10']
+LABEL_NAMES = ['HandStart','FirstDigitTouch','BothStartLoadPhase','LiftOff','Replace','BothReleased']
+
+def load_raw_train_data(subject_id=1,series_id=1):
+	"""
+		Loads a set of data from the training folder
+		returns the training data, training labels and 
+		the (RAW) feature names.
+		@params:
+			subject_id: the ID of the participant
+			series_id: the ID of the recording session
+		@returns:
+			data: n * m numpy array of time ordered data
+			events: n * k labels for each data point 
+	"""
+	file_name = './data/train/subj'+str(subject_id)+'_series'+str(series_id)
+	data = pd.read_csv(file_name+'_data.csv')
+	events = pd.read_csv(file_name+'_events.csv')
+	return data.values[:,1:], events.values[:,1:]
+	
+	
+def load_raw_test_data(subject_id=1,series_id=9):
+	"""
+		Loads a set of data from the testing folder
+		returns the testing data only
+		@params:
+			subject_id: the ID of the participant
+			series_id: the ID of the recording session
+		@returns:
+			data: n * m numpy array of time ordered data
+	"""
+	file_name = './data/test/subj'+str(subject_id)+'_series'+str(series_id)
+	data = pd.read_csv(file_name+'_data.csv')
+	return data.values[:,1:]
+
+
+def data_streamer(mode='train'):
+	"""
+		Generator that streams data according to how it is best necessary
+		Use as follows:
+			ds = data_streamer()
+			data = ds.next() 
+			# do something with data
+			data = ds.next()
+			# do something with data
+			etc etc.
+	"""
+	participants = xrange(1,13) # 12 subjects
+	train_series = xrange(1,9) # 8 series in train
+	test_series = xrange(9,11) # 2 series in test
+	if mode == 'train':
+		for participant in participants:
+			for series in train_series:
+				yield load_raw_train_data(subject_id=participant, series_id=series)
+	elif mode == 'test':
+		for participant in participants:
+			for series in test_series:
+				yield load_raw_test_data(subject_id=participant, series_id=series)
+			
+
+
+
+if __name__ == '__main__':
+	print """
+		To use IOUtils:
+		(1) Download data from https://www.kaggle.com/c/grasp-and-lift-eeg-detection/data
+		(2) Copy 'test.zip' and 'train.zip' into a folder called data in this repository
+		(3) Unzip the two.
+		(4) good to go
+	"""
+
+
---
+++ data/README.md
@@ -0,0 +1,5 @@
+# Obtaining the data
+1. Download data from https://www.kaggle.com/c/grasp-and-lift-eeg-detection/data
+2. Copy 'test.zip' and 'train.zip' into this folder
+3. Unzip the two.
+4. good to go
