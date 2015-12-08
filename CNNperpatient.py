2614c6770cf3b2f44cd64008d505d0a321fac04f
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-12-07 20:58:50 -0500

    Add Neural Network Scripts

---
+++ CNNperpatient.py
@@ -0,0 +1,137 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import numpy as np
+import matplotlib.pylab as plt
+from collections import Counter
+import IOutils
+import random
+# from neuralnetworks.templates import BasicCNN
+from sklearn.metrics import confusion_matrix, classification_report
+import theano
+import lasagne
+from lasagne import layers
+from lasagne.updates import nesterov_momentum
+from nolearn.lasagne import NeuralNet
+from neuralnetworks.tools import LayerFactory
+from preprocessing.preprocess_utils import window_generator_ND
+from metrics.custom import multiple_auc
+
+
+# In[2]:
+
+WINDOW_SIZE = 300
+
+
+# In[3]:
+
+LF = LayerFactory()
+layers_list = [
+    LF(layers.InputLayer, 'input', shape=(None, WINDOW_SIZE, 32 )),
+#     LF(layers.DropoutLayer, p=0.5),
+    LF(layers.Conv1DLayer, 'conv1', num_filters=6, filter_size=5, nonlinearity=None, pad='same'),
+    LF(layers.Conv1DLayer, 'conv2', num_filters=3, filter_size=2),
+    LF(layers.MaxPool1DLayer, 'maxpool1', pool_size=4),
+#     LF(layers.DropoutLayer, 'drop1', p=0.5),
+    LF(layers.DenseLayer, 'dense1', num_units=300),
+    LF(layers.DropoutLayer, 'drop2', p=0.5),
+    LF(layers.DenseLayer, 'out', num_units=12, nonlinearity=lasagne.nonlinearities.sigmoid)
+]
+
+
+# In[4]:
+
+nn = NeuralNet(layers_list, 
+               max_epochs=30, 
+               update=nesterov_momentum, 
+               update_learning_rate=0.02, 
+               verbose=1000, 
+               **LF.kwargs)
+
+
+# In[5]:
+
+training_ds = IOutils.data_streamer(patients_list=[2], series_list=range(1,7))
+# nn = BasicCNN(input_shape=(None,42), output_num_units=12, max_epochs=50, hidden=[256, 120], add_drops=[1,1])
+vt = IOutils.VectorTransformer()
+
+
+# In[ ]:
+
+n_repeat_sampling = 1
+dataset_count = 0
+for X,Y in training_ds:
+    X = X.astype(np.float)
+    X[np.isnan(X)] = 0
+    X = X/X.max()
+    wg = window_generator_ND(X, window_size=WINDOW_SIZE)
+    dataset_count += 1
+    # transform the Ys
+    Y_train = vt.transform(Y)
+    
+    time_point = 0
+    X_train = np.array(list(wg))
+    print X_train.shape
+    any_nans = np.isnan(X_train).any()
+    any_infs = np.isinf(X_train).any()
+
+    if any_nans or any_infs:
+        print('NANS WERE FOUND')
+        break
+#         print X_train.shape
+    # fit the classifier
+    nn.fit(X_train, Y_train)
+    #end windower
+#endfor
+
+
+# In[ ]:
+
+from metrics.custom import multiple_auc
+
+X_test, Y_test = IOutils.data_streamer(patients_list=[2], series_list=[8]).next()
+# X_test, Y_test = testing_ds.next()
+X_test = X_test.astype(np.float)
+X_test[np.isnan(X_test)] = 0
+X_test = X_test/X_test.max()
+
+
+Y_test = vt.transform(Y_test).astype(np.int32).reshape(-1)
+
+wg = window_generator_ND(X_test, window_size=WINDOW_SIZE)
+
+# predicted = nn.predict(X_test)
+
+
+# print confusion_matrix(Y_test, predicted)
+
+
+# # In[ ]:
+
+# print classification_report(Y_test, predicted)
+
+# print multiple_auc(Y_test, predicted)
+
+
+# In[ ]:
+
+testing_data = np.array(list(wg))
+
+
+# In[ ]:
+
+testing_data.shape
+
+import cPickle as pickle
+
+pickle.dump(nn, open('./CNN_perpatient.pickle', 'w'))
+print('Pickle saved')
+print('terminate')
+
+
+# In[ ]:
+
+
+
---
+++ NeuralNetworkPerpatient-Nonzerodistinguishing.py
@@ -0,0 +1,126 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import numpy as np
+import matplotlib.pylab as plt
+from collections import Counter
+import IOutils
+import random
+from neuralnetworks.templates import BasicNN2
+from sklearn.metrics import confusion_matrix, classification_report
+
+
+# In[ ]:
+
+training_ds = IOutils.data_streamer2(keeplist=[ (1, i) for i in range(1,7) ]) # obtain the first 7 datas for the 1st subject
+nn = BasicNN2(input_shape=(None,42), output_num_units=11, max_epochs=100, hidden=[200,120,30])
+vt = IOutils.VectorTransformer()
+
+
+# In[ ]:
+
+n_repeat_sampling = 1
+dataset_count = 0
+for X,Y in training_ds:
+    dataset_count += 1
+    # transform the Ys
+    Y = vt.transform(Y)
+#     print('total size before sampling:', len(Y))
+    X = X.astype(np.float)
+    # normalization for regression
+    X[np.isnan(X)] = 0
+    X = X/X.max()
+    
+    # obtains the zeros in the datasets
+    X_zero = X[Y==0]
+    Y_zero = Y[Y==0]
+#     print('number of zeros:',len(Y_zero))
+    
+    # obtain the nonzeros in the dataset
+    X_nonzero = X[Y>0]
+    Y_nonzero = Y[Y>0].reshape(-1, 1) - 1
+#     print('number of nonzeros', len(Y_nonzero))
+    
+    # obtain the number of zeros to sample
+#     to_sample = int(np.max(Counter(Y.tolist()).values()))
+#     print('number of zeros to sample:',to_sample)
+    
+    for i in range(n_repeat_sampling):
+        # sample the zeros
+#         X_sampled = np.array(random.sample(X_zero.tolist(), to_sample), dtype=X_zero.dtype)
+#         Y_sampled = np.zeros((X_sampled.shape[0], 1))
+        
+        # compile the dataset
+#         X_train = np.vstack((X_sampled, X_nonzero))
+#         Y_train = np.vstack((Y_sampled, Y_nonzero))
+
+        X_train = X_nonzero
+        Y_train = Y_nonzero
+
+        # shuffle the data
+        zipped = zip(X_train,Y_train)
+        random.shuffle(zipped)
+        X_train,Y_train = zip(*zipped)
+        
+        # now all ready!
+        X_train = np.array(X_train, dtype=np.float)
+        Y_train = np.array(Y_train, dtype=np.int32).reshape(-1)
+        
+        any_nans = np.isnan(X_train).any()
+        any_infs = np.isinf(X_train).any()
+        print('Unique Ys:',np.unique(Y_train).tolist())
+        print('Are any Xs nan?', any_nans)
+        print('Are any Xs inf?', any_infs)
+        
+        if any_nans or any_infs:
+            print('NANS WERE FOUND')
+            break
+        # fit the classifier
+        nn.fit(X_train, Y_train)
+
+        print('dataset: ',dataset_count,', trained: ',i,'/',n_repeat_sampling, )
+        #endfor
+    #endfor
+#     print('total size after sampling:',len(Y_train))
+#     print('-----')
+
+
+# In[ ]:
+
+
+
+# In[4]:
+
+testing_ds = IOutils.data_streamer2(keeplist=[(2,8)])
+X_test, Y_test = testing_ds.next()
+X_test = X_test.astype(np.float)
+X_test = X_test.astype(np.float)
+X_test[np.isnan(X_test)] = 0
+X_test = X_test/X_test.max()
+Y_test = vt.transform(Y_test).astype(np.int32)
+
+
+X_test = X_test[Y_test > 0]
+Y_test = Y_test[Y_test > 0] - 1
+
+Y_test.reshape(-1)
+print np.unique(Y_test)
+predicted = nn.predict(X_test)
+
+
+# In[ ]:
+
+print confusion_matrix(Y_test, predicted)
+
+
+# In[ ]:
+
+print classification_report(Y_test, predicted)
+
+
+# In[ ]:
+
+
+
