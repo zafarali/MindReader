6ed240f78c770870351bf8730dcb3b42b7801de7
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2015-12-09 00:17:57 -0500

    A whole load of fixes in an attempt to get #1

--- kaggle_NaiveNN.py
+++ kaggle_NaiveNN.py
@@ -7,17 +7,20 @@ import csv
 import numpy as np
 import scipy
 import pandas
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, classification_report, confusion_matrix
 from numpy import fft
 from numpy.random import randint
 # Lasagne (& friends) imports
 import theano
 from nolearn.lasagne import BatchIterator, NeuralNet
-from lasagne.objectives import aggregate, binary_crossentropy
-from lasagne.layers import InputLayer, DropoutLayer, DenseLayer,Conv1DLayer,MaxPool1DLayer
-from lasagne.updates import nesterov_momentum
+from lasagne.objectives import aggregate, categorical_crossentropy
+from lasagne import layers
+import lasagne
+from lasagne.updates import adagrad
 from theano.tensor.nnet import sigmoid
 import gc
+from metrics.custom import multiple_auc
+from IOutils import VectorTransformer
 
 # Silence some warnings from lasagne
 import warnings
@@ -30,7 +33,7 @@ TRAIN_SERIES = list(range(1,9))
 TEST_SERIES = [9,10]
 
 N_ELECTRODES = 32
-N_EVENTS = 6
+N_EVENTS = 12
 
 # We train on TRAIN_SIZE randomly selected location each "epoch" (yes, that's
 # not really an epoch). One-fifth of these locations are used for validation,
@@ -47,11 +50,16 @@ class Source:
     mean = None
     std = None
 
-    def load_raw_data(self, subject, series):
-        raw_data = [self.read_csv(self.path(subject, i, "data")) for i in series]
+    def load_raw_data(self, subjects, series):
+        raw_data = []
+        if type(subjects) != list:
+            subjects = [ subjects ]
+        for subject in subjects:
+            raw_data.extend([self.read_csv(self.path(subject, i, "data")) for i in series])
+            raw_events = [self.read_csv(self.path(subject, i, "events")) for i in series]
         self.data = np.concatenate(raw_data, axis=0).astype(np.float32)
-        raw_events = [self.read_csv(self.path(subject, i, "events")) for i in series]
-        self.events = np.concatenate(raw_events, axis=0).astype(np.int32)
+        self.data[np.isnan(self.data)] = 0
+        self.events = VectorTransformer().transform(np.concatenate(raw_events, axis=0).astype(np.int32)).reshape(-1, 1)
     
     def normalize(self):
         self.data = self.data - self.mean
@@ -77,10 +85,14 @@ class TrainSource(Source):
         self.load_raw_data(subject, series_list)
         self.mean = self.data.mean(axis=0)
         self.std = self.data.std(axis=0)
+        # print 'mean:',self.mean
+        # print 'sd', self.std
         self.normalize()
-        self.principle_components = scipy.linalg.svd(self.data, full_matrices=False)
-        self.std2 = self.data.std(axis=0)
-        self.data /= self.std2
+        print 'data shapes:', self.data.shape
+        print 'event shapes', self.events.shape
+        # self.principle_components = scipy.linalg.svd(self.data, full_matrices=False)
+        # self.std2 = self.data.std(axis=0)
+        # self.data = self.data / self.std2
 
         
 # Note that Test/Submit sources use the mean/std from the training data.
@@ -93,9 +105,10 @@ class TestSource(Source):
         self.load_raw_data(subject, series)
         self.mean = train_source.mean
         self.std = train_source.std
-        self.principle_components = train_source.principle_components
+        # self.principle_components = train_source.principle_components
         self.normalize()
-        self.data /= train_source.std2
+
+        # self.data = self.data / train_source.std2
         
 
 class SubmitSource(TestSource):
@@ -155,7 +168,7 @@ class IndexBatchIterator(BatchIterator):
             self.augmented[SAMPLE_SIZE-1:] = x
             self.augmented[:SAMPLE_SIZE-1] = x[0]
         self.Xbuf = np.zeros([self.batch_size, N_ELECTRODES, TIME_POINTS], np.float32) 
-        self.Ybuf = np.zeros([self.batch_size, N_EVENTS], np.float32) 
+        self.Ybuf = np.zeros(self.batch_size, np.int32) 
     
     def transform(self, X_indices, y_indices):
         X_indices, y_indices = super(IndexBatchIterator, self).transform(X_indices, y_indices)
@@ -169,15 +182,26 @@ class IndexBatchIterator(BatchIterator):
             sample = self.augmented[ndx:ndx+SAMPLE_SIZE]
             # Reverse so we get most recent point, otherwise downsampling drops the last
             # DOWNSAMPLE-1 points.
+            # print '---'
+            # print 'i=',i
+            # print 'ndx=',ndx
+            # print 'X[i].shape=',X[i].shape
+            
+            # print 'sample.shape=',sample.shape
+            # print 'sample[::-1].shape=',sample[::-1].shape
+
+            # print 'sample[::-1][::DOWNSAMPLE].shape=',sample[::-1][::DOWNSAMPLE].shape
             X[i] = sample[::-1][::DOWNSAMPLE].transpose()
             if y_indices is not None:
                 Y[i] = self.source.events[ndx]
+
         Y = None if (y_indices is None) else Y
+
         return X, Y
     
 
 # Simple / Naive net. Borrows from Daniel Nouri's Facial Keypoint Detection Tutorial 
-    
+
 def create_net(train_source, test_source, batch_size=128, max_epochs=20): 
     
     batch_iter_train = IndexBatchIterator(train_source, batch_size=batch_size)
@@ -186,43 +210,49 @@ def create_net(train_source, test_source, batch_size=128, max_epochs=20):
 
     dense = 196 # larger (1024 perhaps) would be better
     
-    layers = [
-        LF(InputLayer, shape=(None, N_ELECTRODES, TIME_POINTS)), 
-        LF(DropoutLayer, p=0.5),
-        # This first layer condenses N_ELECTRODES down to num_filters.
-        # Since the electrode results are reportedly highly reduntant this
-        # should speed things up without sacrificing accuracy. It may
-        # also increase stability. This was 8 in an earlier version.
-        LF(Conv1DLayer, num_filters=4, filter_size=1, nonlinearity=None),
-        # Try one convolutional layer
-        LF(Conv1DLayer, num_filters=8, filter_size=5, border_mode="same"),
-        # Maxpooling is more typically done with a pool_size of 2
-        LF(MaxPool1DLayer, pool_size=4),
-        # Standard fully connected net from here on out.
-        LF(DropoutLayer, p=0.5),
-        LF(DenseLayer, num_units=dense),
-        LF(DropoutLayer, p=0.5),
-        LF(DenseLayer, num_units=dense),
-        LF(DropoutLayer, p=0.5),
-        LF(DenseLayer, layer_name="output", num_units=N_EVENTS, nonlinearity=sigmoid)
+    layer_list = [
+        LF(layers.InputLayer, shape=(None, N_ELECTRODES, TIME_POINTS)), 
+        # LF(DropoutLayer, p=0.5),
+        # # This first layer condenses N_ELECTRODES down to num_filters.
+        # # Since the electrode results are reportedly highly reduntant this
+        # # should speed things up without sacrificing accuracy. It may
+        # # also increase stability. This was 8 in an earlier version.
+        # LF(Conv1DLayer, num_filters=4, filter_size=1, nonlinearity=None),
+        # # Try one convolutional layer
+        # LF(Conv1DLayer, num_filters=8, filter_size=5, border_mode="same"),
+        # # Maxpooling is more typically done with a pool_size of 2
+        # LF(MaxPool1DLayer, pool_size=4),
+        # # Standard fully connected net from here on out.
+        # LF(DropoutLayer, p=0.5),
+        # LF(DenseLayer, num_units=dense),
+        # LF(DropoutLayer, p=0.5),
+        # LF(DenseLayer, num_units=dense),
+        # LF(DropoutLayer, p=0.5),
+
+
+        LF(layers.Conv1DLayer, 'conv1', num_filters=6, filter_size=5, nonlinearity=None, pad='same'),
+        LF(layers.Conv1DLayer, 'conv2', num_filters=3, filter_size=2),
+        LF(layers.MaxPool1DLayer, 'maxpool1', pool_size=4),
+        LF(layers.DenseLayer, 'dense1', num_units=300),
+        LF(layers.DropoutLayer, 'drop2', p=0.5),
+
+        LF(layers.DenseLayer, layer_name="output", num_units=N_EVENTS, nonlinearity=lasagne.nonlinearities.softmax)
     ]
     
-    def loss(x,t):
-        return aggregate(binary_crossentropy(x, t))
+    # def loss(x,t):
+    #     return aggregate(categorical_crossentropy(x, t))
     
     
     nnet =  NeuralNet(
-        y_tensor_type = theano.tensor.matrix,
-        layers = layers,
+        # y_tensor_type = theano.tensor.matrix,
+        layers = layer_list,
         batch_iterator_train = batch_iter_train,
         batch_iterator_test = batch_iter_test,
         max_epochs=max_epochs,
-        verbose=1,
-        update = nesterov_momentum, 
-        update_learning_rate = 0.02,
-        update_momentum = 0.9,
-        objective_loss_function = loss,
-        regression = True,
+        verbose=1000000,
+        update = adagrad, 
+        update_learning_rate = 0.01,
+        # update_momentum = 0.5,
         **LF.kwargs
         )
 
@@ -249,17 +279,23 @@ def score(net, samples=256):
     return roc_auc_score(actual.reshape(-1), predicted.reshape(-1))
     
 
-def train(factory, subj, max_epochs=20, valid_series=[1,2], params=None):
-    tseries = sorted(set(TRAIN_SERIES) - set(valid_series))
+def train(factory, subj, series=TRAIN_SERIES, max_epochs=20, valid_series=[1,2], params=None):
+    tseries = sorted(set(series) - set(valid_series))
     train_source = TrainSource(subj, tseries)
-    test_source = TestSource(subj, valid_series, train_source)
+    test_source = TestSource(subj, valid_series, train_source) # just a place holder
+    # i don't think it really does anything when line 265 is commented out
     net = factory(train_source, test_source, max_epochs=max_epochs)
     if params is not None:
         net.load_params_from(params)
     net.fit(train_indices, train_indices)
-    print("Score:", score(net))
+    # print("Score:", score(net))
     return (net, train_source)
  
+# def get_predictions(factory, params, subj, series):
+#     train_source = TrainSource(subj, series)
+#     test_source = TestSource(subj, valid_series, train_source)
+
+#     net = factory()
 
 def train_all(factory, max_epochs=30, init_epochs=30, valid_series=[1,2]):
     info = {}
@@ -272,7 +308,61 @@ def train_all(factory, max_epochs=30, init_epochs=30, valid_series=[1,2]):
         info[subj] = (params, train_source)
         init_epochs = 0
     return (factory, info)   
-  
+
+from collections import Counter
+
+def train_subject_specific(factory, subject_id, train_series_ids, test_series_ids, max_epochs=30, init_epochs=0, train_sample_size=0):
+    info = {}
+    params = None
+    # toval = int(np.random.choice(train_series_ids))
+    # print toval
+    toval = 1 # for now fix this
+    # print train_series_ids
+    train_series_ids = sorted(set(train_series_ids) - set([toval]))
+    # print train_series_ids
+    train_source = TrainSource(subject_id, train_series_ids)
+    test_source = TestSource(subject_id, [toval] , train_source) # just a place holder
+    # i don't think it really does anything when line 265 is commented out
+    net = factory(train_source, test_source, max_epochs=max_epochs)
+    
+    if train_sample_size == 0:
+        train_sample_size = len(train_source.events)
+    
+    train_indices = np.zeros(train_sample_size, dtype=np.int32) -1
+    
+    # print Counter(train_source.events.T.tolist())
+
+    train_net = factory(train_source, test_source, max_epochs=max_epochs)
+
+    print train_indices.dtype
+    train_net.fit(train_indices, train_indices)
+
+    params = train_net.get_all_params_values()
+    # get rid of it from memory
+    del train_source
+
+
+    # net.predict_proba
+    val_source = TrainSource(subject_id, test_series_ids)
+    # source = net.batch_iterator_test.source
+
+    val_indices = np.arange(len(val_source.events))
+
+    print len(val_source.events)
+    raw_input()
+    test_net = factory(val_source, test_source)
+
+    test_net.load_weights_from(params)
+
+    predicted = test_net.predict(val_indices)
+    actual = val_source.events
+
+    print 'unique predicted labels:',np.unique(predicted)
+    print 'unique actual labels',np.unique(actual)
+
+    return actual, predicted
+    # train(factory, subject_id, )
+
  
 def make_submission(train_info, name):
     factory, info = train_info
@@ -293,5 +383,10 @@ def make_submission(train_info, name):
         
         
 if __name__ == "__main__":
-    train_info = train_all(create_net, max_epochs=25) # Training for longer would likley be better
-    make_submission(train_info, "naive_grasp.csv") 
+    # train_info = train_all(create_net, max_epochs=25) # Training for longer would likley be better
+    Y_true, Y_pred = train_subject_specific(create_net, subject_id=[ 2 ], train_series_ids=range(2,9), \
+        test_series_ids=range(1,2), train_sample_size=50, max_epochs=10)
+    print 'multiple_auc:',multiple_auc(Y_true, Y_pred)
+    print classification_report(Y_true, Y_pred)
+    print confusion_matrix(Y_true, Y_pred)
+    # make_submission(train_info, "naive_grasp.csv") 
